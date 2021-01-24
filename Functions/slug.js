7c32c8e1011b9d8c72258e69f35f225e6fcd3fc8
davidcraig <davidcraig1986@gmail.com>
2021-01-24 15:00:42 +0000

    Add final space removal.

--- Functions/slug.js
+++ Functions/slug.js
@@ -1,12 +1,14 @@
 function slug(string) {
   return string
     .replace(' ', '_')
+    .replace('+', '')
     .replace('(', '')
     .replace(')', '')
     .replace("\r\n", '')
     .replace("\r", '')
     .replace("\n", '')
     .replace('?', '')
+    .replace(' ', '')
     .toLowerCase();
 }
 
