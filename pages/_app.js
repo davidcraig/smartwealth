2790e34a1c31ee3919bcd1089b0facae87a79aa8
davidcraig <davidcraig1986@gmail.com>
2021-01-24 14:16:27 +0000

    Create use effect that triggers spreadsheet worker to retrieve data.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
+import React, { useState, useEffect } from 'react';
 import '../styles/app.scss'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [stocks, setStocks] = useState([]);
+
+  useEffect(() => {
+    const spreadsheetUrl = 'https://spreadsheets.google.com/feeds/cells/1sSOTCWajfq_t0SEMFhfR0JedhgGXNeIH0ULMA2310c0/1/public/values?alt=json'
+    const SpreadsheetWorker = new Worker('/js/spreadsheet.js')
+
+    console.log('sending worker parse request')
+    SpreadsheetWorker.postMessage({ type: 'parse', url: spreadsheetUrl, headerRow: 3 })
+    SpreadsheetWorker.onmessage = (e => {
+      if (e.data.type === 'parse-result') {
+        setStocks(e.data.data)
+      }
+    })
+  }, []);
+
+  return <Component {...pageProps} stocks={stocks} />
 }
 
 export default MyApp
