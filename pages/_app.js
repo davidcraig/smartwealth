a0d6316d901a585421c73752158387d7bbbd4f31
davidcraig <davidcraig1986@gmail.com>
2021-01-24 14:33:31 +0000

    Store stocks in localstorage.

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import '../styles/app.scss'
 
+
 function MyApp({ Component, pageProps }) {
   const [stocks, setStocks] = useState([]);
 
   useEffect(() => {
     const spreadsheetUrl = 'https://spreadsheets.google.com/feeds/cells/1sSOTCWajfq_t0SEMFhfR0JedhgGXNeIH0ULMA2310c0/1/public/values?alt=json'
     const SpreadsheetWorker = new Worker('/js/spreadsheet.js')
+    let store = localStorage
 
-    console.log('sending worker parse request')
-    SpreadsheetWorker.postMessage({ type: 'parse', url: spreadsheetUrl, headerRow: 3 })
+    let stocks = JSON.parse(store.getItem('stocks'))
+
+    if (stocks == null) {
+      console.log('sending worker parse request')
+      SpreadsheetWorker.postMessage({ type: 'parse', url: spreadsheetUrl, headerRow: 3 })
+    } else {
+      setStocks(stocks)
+    }
+    
     SpreadsheetWorker.onmessage = (e => {
       if (e.data.type === 'parse-result') {
+        store.setItem('stocks', JSON.stringify(e.data.data))
         setStocks(e.data.data)
       }
     })
