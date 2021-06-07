c06a6a9c486c2bb384daf641ddf577937fe6f498
davidcraig <davidcraig1986@gmail.com>
2021-06-07 15:59:45 +0100

    Update app js.

--- pages/_app.js
+++ pages/_app.js
@@ -1,33 +1,54 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 import '../styles/app.scss'
 
-
 function MyApp({ Component, pageProps }) {
-  const [stocks, setStocks] = useState([]);
+  const [stocks, setStocks] = useState([])
+  const [positionsHeld, setPositionsHeld] = useState([])
+  const [currentValue, setCurrentValue] = useState([])
 
+  // Load Stocks from SmartWealth public spreadsheet
   useEffect(() => {
     const spreadsheetUrl = 'https://spreadsheets.google.com/feeds/cells/1sSOTCWajfq_t0SEMFhfR0JedhgGXNeIH0ULMA2310c0/1/public/values?alt=json'
     const SpreadsheetWorker = new Worker('/js/spreadsheet.js')
     let store = localStorage
 
-    let stocks = JSON.parse(store.getItem('stocks'))
+    const stocks = JSON.parse(store.getItem('stocks'))
 
     if (stocks == null) {
-      console.log('sending worker parse request')
       SpreadsheetWorker.postMessage({ type: 'parse', url: spreadsheetUrl, headerRow: 3 })
     } else {
       setStocks(stocks)
     }
-    
+
     SpreadsheetWorker.onmessage = (e => {
       if (e.data.type === 'parse-result') {
         store.setItem('stocks', JSON.stringify(e.data.data))
         setStocks(e.data.data)
       }
     })
-  }, []);
+  }, [])
 
-  return <Component {...pageProps} stocks={stocks} />
+  // Load any held positions from localStorage.
+  useEffect(() => {
+    let store = localStorage
+
+    let positions = store.getItem('positions')
+
+    if (positions == null) {
+      //
+    } else {
+      setPositionsHeld(JSON.parse(positions))
+    }
+  }, [])
+
+  return (
+    <Component
+      {...pageProps}
+      stocks={stocks}
+      positionsHeld={positionsHeld}
+      setPositionsHeld={setPositionsHeld}
+    />
+  )
 }
 
 export default MyApp
