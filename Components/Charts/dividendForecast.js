d746f394883a1ba1c2f5b453e8cc7984731fa496
davidcraig <davidcraig1986@gmail.com>
2021-06-07 15:59:08 +0100

    Copy dividend forecast chart

---
+++ Components/Charts/dividendForecast.js
@@ -0,0 +1,84 @@
+import React from 'react'
+import NextHighchart from '../NextHighchart'
+
+const themeColour1 = '#2e313b'
+const themeColour2 = '#494e5c'
+const wowOkColour = '#3292f1'
+const wowEpicColour = '#c56fff'
+
+const chartOptions = (dividendData, months) => {
+  // We need to create dividendData
+  const series = []
+  Object.keys(dividendData).map(company => {
+    series.push({
+      name: company,
+      data: dividendData[company],
+      borderWidth: 0
+    })
+  })
+
+  // Final chart data
+  return {
+    chart: {
+      type: 'column'
+    },
+    title: {
+      text: 'Dividends Forecast'
+    },
+    xAxis: {
+      categories: months
+    },
+    tooltip: {
+      backgroundColor: '#111111',
+      pointFormat: "{series.name}<br/>£{point.y:.2f}"
+    },
+    yAxis: {
+      stackLabels: {
+        enabled: false
+      },
+      gridLineColor: themeColour2,
+      minorGridLineColor: '#2a2d35',
+      tickInterval: 100,
+      minorTicks: true,
+      title: {
+        text: 'Dividend'
+      },
+    },
+    plotLines: [{
+      color: 'green', // Color value
+      dashStyle: 'longdashdot', // Style of the plot line. Default to solid
+      value: 2000, // Value of where the line will appear
+      width: 1 // Width of the line    
+    }],
+    legend: {
+      enabled: false
+    },
+    plotOptions: {
+      column: {
+        stacking: 'normal',
+        dataLabels: {
+          enabled: false,
+        }
+      },
+      colors: undefined,
+      showInLegend: false
+    },
+    series
+  }
+}
+
+const dividendForecast = (forecast) => {
+  if (forecast) {
+    const options = chartOptions(forecast.dividendData, forecast.months)
+
+    return (
+      <NextHighchart
+        options={options}
+      />
+    )
+  }
+
+  return ''
+}
+
+export default dividendForecast
