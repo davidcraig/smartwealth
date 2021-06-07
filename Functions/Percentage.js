02a020f24e7085a883cea6e9ed8fa1048ce5530f
davidcraig <davidcraig1986@gmail.com>
2021-06-07 15:59:30 +0100

    Create percentage formatter helper.

---
+++ Functions/Percentage.js
@@ -0,0 +1,10 @@
+/**
+ * Formats a number as a percentage
+ * @param {*} number Number to be formatted.
+ * @returns string
+ */
+export function Percentage (number) {
+  return number.toFixed(2)
+}
+
+export default Percentage
