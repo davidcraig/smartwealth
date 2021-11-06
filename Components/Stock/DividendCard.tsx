b1ca588bc5cecb5d71eb184acc056b6f4c7e7b7e
davidcraig <davidcraig1986@gmail.com>
2021-11-06 13:46:35 +0000

    Update stock page to include goal timeline tables.

--- Components/Stock/DividendCard.tsx
+++ Components/Stock/DividendCard.tsx
@@ -12,39 +12,27 @@ interface DividendCardProps {
 function DividendCard ({ stock }: DividendCardProps) {
   const sharesFor10PerMonth = 120 / GetAnnualDividendAsGBP(stock)
   const sharesFor100PerMonth = 1200 / GetAnnualDividendAsGBP(stock)
+  const sharesFor1000PerMonth = sharesFor100PerMonth * 10
   const amountFor10PerMonth = sharesFor10PerMonth * GetSharePrice(stock)
   const amountFor100PerMonth = sharesFor100PerMonth * GetSharePrice(stock)
+  const amountFor1000PerMonth = amountFor100PerMonth * 10
 
   return (
     <Card className='is-compact' title='Dividend Information'>
-      <table className='table is-narrow has-no-borders'>
-        <tbody>
-          <tr>
-            <th>Last Dividend</th>
-            <th>Annual Dividend</th>
-            <th>Dividend Yield (US)</th>
-            <th>Dividend Yield (UK)</th>
-          </tr>
-          <tr>
-            <td>{stock.last_dividend_amount}</td>
-            <td>{stock.dividend_return}</td>
-            <td>{stock.dividend_yield}</td>
-            <td>{stock.currency === 'USD' ? `${(parseFloat(stock.dividend_yield) * 0.85).toFixed(2)}%` : stock.dividend_yield}</td>
-          </tr>
-          <tr>
-            <th>Stocks for £10/mo</th>
-            <th>Amount Invested for £10/mo</th>
-            <th>Stocks for £100/mo</th>
-            <th>Amount Invested for £100/mo</th>
-          </tr>
-          <tr>
-            <td>{(sharesFor10PerMonth).toFixed(4)}</td>
-            <td>{BaseCurrency(amountFor10PerMonth)}</td>
-            <td>{(sharesFor100PerMonth).toFixed(4)}</td>
-            <td>{BaseCurrency(amountFor100PerMonth)}</td>
-          </tr>
-        </tbody>
-      </table>
+      <div className='grid'>
+        <div>
+          Stocks for £10/mo: {(sharesFor10PerMonth).toFixed(4)}
+        </div>
+        <div>
+          Amount Invested for £10/mo: {BaseCurrency(amountFor10PerMonth)}
+        </div>
+        <div>
+          Stocks for £100/mo: {(sharesFor100PerMonth).toFixed(4)}
+        </div>
+        <div>
+          Amount Invested for £100/mo: {BaseCurrency(amountFor100PerMonth)}
+        </div>
+      </div>
     </Card>
   )
 }
--- pages/stock/[ticker].tsx
+++ pages/stock/[ticker].tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import { useRouter } from 'next/router'
+import { connect } from 'react-redux'
 import React, { useState, ReactFragment } from 'react'
 import Navbar from '../../Components/Navbar'
 import { Columns, Column, Card } from '@davidcraig/react-bulma'
@@ -7,6 +8,13 @@ import StockInterface from '../../types/Stock'
 import GetSharePrice from '../../Functions/Stock/GetSharePrice'
 import GetAnnualDividendAsGBP from '../../Functions/Stock/GetAnnualDividendAsGBP'
 import DividendCard from '../../Components/Stock/DividendCard'
+
+function displayTimeToReachGoal (months) {
+  if (months > 12) {
+    return `${Math.floor(months / 12)} years`
+  }
+  return `${months.toFixed(1)} m`
+}
 
 function StockView ({ stocks }) {
   const router = useRouter()
@@ -42,8 +50,8 @@ function StockView ({ stocks }) {
 
   const sharesFor10PerMonth = 120 / GetAnnualDividendAsGBP(stock)
   const sharesFor100PerMonth = 1200 / GetAnnualDividendAsGBP(stock)
-  const amountFor10PerMonth = sharesFor10PerMonth * GetSharePrice(stock)
   const amountFor100PerMonth = sharesFor100PerMonth * GetSharePrice(stock)
+  const amountFor1000PerMonth = amountFor100PerMonth * 10
 
   return (
     <React.Fragment>
@@ -58,13 +66,62 @@ function StockView ({ stocks }) {
         <div className='content'>
           <h1 className='h1'>[{stock.ticker}] {stock.name} - {stock.currency === 'USD' ? "$" : "£"}{stock.share_price}</h1>
           <Columns>
-            <Column class='is-one-half'>
+            <Column class='is-two-thirds'>
               <DividendCard stock={stock} />
             </Column>
-            <Column class='is-one-half'>
-              <h1>TODO</h1>
+            <Column class='is-one-third'>
+              <Card title='Dividend Data'>
+                <p>Last Dividend: {stock.last_dividend_amount}</p>
+                <p>Dividend Yield: {stock.dividend_yield}</p>
+              </Card>
             </Column>
           </Columns>
+          <Card title='Time to reach income goals'>
+            <table className='table'>
+              <thead>
+                <th></th>
+                <th>@1000/Mo</th>
+                <th>@1200/Mo</th>
+                <th>@1300/Mo</th>
+                <th>@1500/Mo</th>
+                <th>@2000/Mo</th>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>£100/Mo</td>
+                  <td>{displayTimeToReachGoal(amountFor100PerMonth / 1000)}</td>
+                  <td>{displayTimeToReachGoal(amountFor100PerMonth / 1200)}</td>
+                  <td>{displayTimeToReachGoal(amountFor100PerMonth / 1300)}</td>
+                  <td>{displayTimeToReachGoal(amountFor100PerMonth / 1500)}</td>
+                  <td>{displayTimeToReachGoal(amountFor100PerMonth / 2000)}</td>
+                </tr>
+                <tr>
+                  <td>£500/Mo</td>
+                  <td>{displayTimeToReachGoal((5 * amountFor100PerMonth) / 1000)}</td>
+                  <td>{displayTimeToReachGoal((5 * amountFor100PerMonth) / 1200)}</td>
+                  <td>{displayTimeToReachGoal((5 * amountFor100PerMonth) / 1300)}</td>
+                  <td>{displayTimeToReachGoal((5 * amountFor100PerMonth) / 1500)}</td>
+                  <td>{displayTimeToReachGoal((5 * amountFor100PerMonth) / 2000)}</td>
+                </tr>
+                <tr>
+                  <td>£1000/Mo</td>
+                  <td>{displayTimeToReachGoal((10 * amountFor100PerMonth) / 1000)}</td>
+                  <td>{displayTimeToReachGoal((10 * amountFor100PerMonth) / 1200)}</td>
+                  <td>{displayTimeToReachGoal((10 * amountFor100PerMonth) / 1300)}</td>
+                  <td>{displayTimeToReachGoal((10 * amountFor100PerMonth) / 1500)}</td>
+                  <td>{displayTimeToReachGoal((10 * amountFor100PerMonth) / 2000)}</td>
+                </tr>
+                <tr>
+                  <td>£1500/Mo</td>
+                  <td>{displayTimeToReachGoal((15 * amountFor100PerMonth) / 1000)}</td>
+                  <td>{displayTimeToReachGoal((15 * amountFor100PerMonth) / 1200)}</td>
+                  <td>{displayTimeToReachGoal((15 * amountFor100PerMonth) / 1300)}</td>
+                  <td>{displayTimeToReachGoal((15 * amountFor100PerMonth) / 1500)}</td>
+                  <td>{displayTimeToReachGoal((15 * amountFor100PerMonth) / 2000)}</td>
+                </tr>
+              </tbody>
+            </table>
+          </Card>
           
         </div>
       </main>
@@ -72,4 +129,8 @@ function StockView ({ stocks }) {
   )
 }
 
-export default StockView
+const mapStateToProps = state => ({
+  stocks: state.stocks.data
+})
+
+export default connect(mapStateToProps)(StockView)
