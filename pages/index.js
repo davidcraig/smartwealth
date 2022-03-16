de9fabe4a5987482a155673792dab18bc7958962
davidcraig <davidcraig1986@gmail.com>
2022-03-16 23:30:49 +0000

    Remove references to legacy pies and positions from index.

--- pages/index.js
+++ pages/index.js
@@ -120,17 +120,20 @@ const calculatePieYields = (pie, stocks) => {
 }
 
 function PortfolioValue ({ accounts, stocks }) {
-  return BaseCurrency(accounts.reduce((prev, account) => {
-    return account.positions.map(pos => {
-      const stock = GetStock(pos.ticker, stocks)
+  return BaseCurrency(
+    accounts.length > 0
+      ? accounts.reduce((prev, account) => {
+        return account.positions.map(pos => {
+          const stock = GetStock(pos.ticker, stocks)
 
-      if (typeof prev === 'object') {
-        return GetPositionValue(pos, stock)
-      }
+          if (typeof prev === 'object') {
+            return GetPositionValue(pos, stock)
+          }
 
-      return prev + GetPositionValue(pos, stock)
-    })
-  }))
+          return prev + GetPositionValue(pos, stock)
+        })
+      }) : 0
+  )
 }
 
 function dividendSumForForecastKey (forecast, forecastKey) {
@@ -178,8 +181,6 @@ const nextFortyYearsNetWorth = (forecast, stocks) => netWorthForForecastKey(fore
 
 export function SmartWealth ({ accounts, positionsHeld, stocks, ...props }) {
   const [forecast, setForecast] = useState([])
-  const [pies, setPies] = useState([])
-  const [pieContributions, setPieContributions] = useState([]) // eslint-disable-line no-unused-vars
   const [forecastLog, setForecastLog] = useState([])
   const [isForecasting, setIsForecasting] = useState(false)
   const canForecast = false
@@ -190,14 +191,13 @@ export function SmartWealth ({ accounts, positionsHeld, stocks, ...props }) {
 
     if (
       !isForecasting &&
-      positionsHeld &&
-      positionsHeld.length > 0 &&
+      accounts.length > 0 &&
       canForecast
     ) {
       CalculationWorker.postMessage({
         type: 'perform-forecast',
-        positions: updatePositions(positionsHeld),
-        pieContributions: pieContributions
+        accounts: accounts,
+        stocks: stocks
       })
       setIsForecasting(true)
     }
@@ -217,19 +217,19 @@ export function SmartWealth ({ accounts, positionsHeld, stocks, ...props }) {
           setForecastLog(log)
           break
         }
-        case 'pie-data': {
-          const pieData = data
-          const pies = Object.keys(pieData).map(key => {
-            const pie = pieData[key]
-            return {
-              name: key,
-              holdings: pie.holdings,
-              positions: pie.positions
-            }
-          })
-          setPies(pies)
-          break
-        }
+        // case 'pie-data': {
+        //   const pieData = data
+        //   const pies = Object.keys(pieData).map(key => {
+        //     const pie = pieData[key]
+        //     return {
+        //       name: key,
+        //       holdings: pie.holdings,
+        //       positions: pie.positions
+        //     }
+        //   })
+        //   setPies(pies)
+        //   break
+        // }
         case 'forecast-results': {
           const results = JSON.parse(data)
           setIsForecasting(false)
@@ -248,43 +248,22 @@ export function SmartWealth ({ accounts, positionsHeld, stocks, ...props }) {
     }
 
     /* Trigger the forecast */
-  }, [positionsHeld, pieContributions])
+  }, [accounts, stocks])
 
   const forecastOutput = ForecastContent(forecast, forecastLog, stocks)
 
-  function updatePositions (positions) {
-    return positions.map(position => {
-      const liveStock = GetStock(position.stock.ticker, stocks)
-      if (liveStock) {
-        position.stock = liveStock
-      }
-      return position
-    })
-  }
-
   function updatePieMonthlyContributions () {
-    const pieContributions = []
     setForecast([])
-    pies.forEach(pie => {
-      pieContributions.push({
-        name: pie.name,
-        monthlyContribution: pie.monthlyContribution || 0
-      })
-    })
     // setPieContributions(pieContributions)
 
     window.CalculationWorker.postMessage({
       type: 'perform-forecast',
-      positions: updatePositions(positionsHeld),
-      pieContributions: pieContributions
+      accounts: accounts,
+      stocks: stocks
     })
   }
 
   const hasPositions = true // positionsHeld && positionsHeld.length > 0
-
-  pies.sort((a, b) => {
-    return a.name.localeCompare(b.name)
-  })
 
   return (
     <div>
@@ -311,45 +290,6 @@ export function SmartWealth ({ accounts, positionsHeld, stocks, ...props }) {
               (hasPositions) && (
                 <Column class='is-one-quarter'>
                   <Card className='is-compact' title='Forecasting'>
-                    {/* Legacy */}
-                    {
-                      pies && pies.length > 0 && (
-                        <table className='table is-compact pie-forecast-controls'>
-                          <thead>
-                            <tr>
-                              <th>Pie</th>
-                            </tr>
-                          </thead>
-                          <tbody>
-                            {
-                              pies.map(pie => {
-                                const [pieAvg, pieYield] = calculatePieYields(pie, stocks)
-
-                                return (
-                                  <Fragment key={pie.name}>
-                                    <tr key={pie.name}>
-                                      <td>{pie.name} <span style={{ float: 'right' }}>avg.yld: {pieAvg} pie.yld: {pieYield}</span></td>
-                                    </tr>
-                                    <tr>
-                                      <td>
-                                        <input
-                                          placeholder='£ / month, blank = 0'
-                                          data-pie={pie.name}
-                                          onChange={(e) => {
-                                            pie.monthlyContribution = parseFloat(e.target.value)
-                                          }}
-                                        />
-                                      </td>
-                                    </tr>
-                                  </Fragment>
-                                )
-                              })
-                            }
-                          </tbody>
-                        </table>
-                      )
-                    }
-
                     {/* Accounts */}
                     <table className='table is-compact pie-forecast-controls'>
                       <thead>
@@ -361,7 +301,6 @@ export function SmartWealth ({ accounts, positionsHeld, stocks, ...props }) {
                         {/* Account Based */}
                         {
                           accounts && accounts.length > 0 && accounts.map(account => {
-                            console.log(account)
                             if (!('pies' in account)) {
                               return false
                             }
@@ -374,7 +313,6 @@ export function SmartWealth ({ accounts, positionsHeld, stocks, ...props }) {
                                 </tr>
                                 {
                                   accountPies.map(pie => {
-                                    console.log(pie)
                                     const [pieAvg, pieYield] = calculatePieYields(pie, stocks)
 
                                     return (
